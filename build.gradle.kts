3869714d9e06e929622ae9c349db726ca4ea9bf4
Silveira, Joao (EFS-GH5, extern) <silveira@itemis.de>
2022-05-04 16:02:01 +0200

    bumped project version to 0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins { `maven-publish` }
 
 group = "org.modelix.mps.api-gen"
 
-var projectVersion = "0.7"
+var projectVersion = "0.8"
 subprojects {
     group = "org.modelix.mps.api-gen"
     version = if(System.getenv("CI") != null) {
